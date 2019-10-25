cf57eb2096660403337f0d31e8d2c0aace97ecd1
Miles Richardson <milesrichardson@gmail.com>
2019-10-25 03:38:02 +0100

    Move doc files into new @splitgraph/content package

--- docs/plugins/transpileModulesConfig.js
+++ docs/plugins/transpileModulesConfig.js
@@ -1,5 +1,5 @@
 // Our modules are managed by yarn workspaces
-const ourModules = ["@splitgraph/design", "@splitgraph/api-client"];
+const ourModules = ["@splitgraph/design", "@splitgraph/content"];
 
 // Enumerate every dependency with an "es" or "es6" module available
 // TODO: Make this programmatic (as in, with javascript, not shell)
