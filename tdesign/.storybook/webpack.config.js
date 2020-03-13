768e0033e5103df8c97e69f254b5244be9c9182c
Miles Richardson <milesrichardson@gmail.com>
2020-03-13 01:43:27 +0000

    Cleanup scripts/packaging
    - Parameterize storybook webpack config for file timeout (useful for waiting
      for TS compilation to finish) and HMR client path (useful for routing behind nginx)
    - Add `clean.sh` script to run before `watch` in `@splitgraph/tlib` and `@splitgraph/tdesign`,
      which clears the `dist` directory, as this seems to help with a problem in docker environments
      when restarting a container with an existing `dist` directory.
    - (todo: Replace `./clean.sh` with cross-platform `rimraf` or similar)

--- tdesign/.storybook/webpack.config.js
+++ tdesign/.storybook/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const process = require('process');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.tsx?$/,
@@ -12,16 +14,31 @@ module.exports = ({ config }) => {
     ],
   });
 
-  const hmrPath = '/__/storybook/__webpack_hmr';
-  config.entry = config.entry.map(x =>
-    x.includes('webpack-hot-middleware/client.js') ? `${x}&path=${hmrPath}` : x
-  );
+  // Set WEBPACK_HMR_CLIENT_PATH in order for HMR to work behind reverse proxy with subpath
+  // e.g. '/__/storybook/__webpack_hmr'
+  if (process.env.WEBPACK_HMR_CLIENT_PATH) {
+    config.entry = config.entry.map(x =>
+      x.includes('webpack-hot-middleware/client.js')
+        ? `${x}&path=${process.env.WEBPACK_HMR_CLIENT_PATH}`
+        : x
+    );
+  }
 
   if (!config.watchOptions) {
     config.watchOptions = {};
   }
 
-  config.watchOptions.aggregateTimeout = 5000;
+  // There is an issue where webpack will attempt to import the compiled typescript
+  // before typescript is finished compiling it. In this case, webpack will display
+  // an error and will not recompile the errored code without a restart of storybook.
+  // Therefore, the best option is to set this timeout to 5 seconds, which is
+  // hopefully enough time to reload before the typescript is completed building.
+  if (process.env.WEBPACK_HMR_AGGREGATE_TIMEOUT) {
+    config.watchOptions.aggregateTimeout = parseInt(
+      process.env.WEBPACK_HMR_AGGREGATE_TIMEOUT,
+      10
+    );
+  }
 
   config.resolve.extensions.push('.ts', '.tsx');
   config.resolve.alias = Object.assign(config.resolve.alias, {
