3185b42af1ffc4e2b5906fb93777cc1f05531de2
Miles Richardson <milesrichardson@gmail.com>
2020-04-09 15:56:35 +0000

    Allow specifying size of icon in `BaseIcon` component
    - svg is default 24px (hardcoded into most of the cssSvg strings)
    - Scale svg with backgroundSize

--- tdesign/src/Icon/BaseIcon.tsx
+++ tdesign/src/Icon/BaseIcon.tsx
@@ -4,24 +4,27 @@ import { jsx, Text } from 'theme-ui';
 import * as React from 'react';
 
 export interface IIconProps {
+  size?: string;
   sx?: any;
 }
 
 export interface IBaseIconProps {
   svgURI: string;
   iconSlug: string;
+  size?: string;
   sx?: any;
 }
 
-export default ({ iconSlug, svgURI, sx }: IBaseIconProps) => {
+export default ({ size = '24px', iconSlug, svgURI, sx }: IBaseIconProps) => {
   return (
     <Text
       className={`sg-icon-${iconSlug}`}
       sx={{
         backgroundImage: `url("${svgURI}")`,
         backgroundRepeat: 'no-repeat',
-        minWidth: '24px',
-        minHeight: '24px',
+        backgroundSize: size,
+        minWidth: size,
+        minHeight: size,
         ...sx,
       }}
     >
