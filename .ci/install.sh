6f3adcdb88b5913d3c5257bf3ae7270215b71cc0
Miles Richardson <milesrichardson@gmail.com>
2020-04-30 23:58:16 +0000

    Allow clearing cache with commit message [clear cache]
    (Trying to fix error in last build violating immutable install?)

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,6 +4,12 @@ CI_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 SPLITGRAPH_DIR="$CI_DIR/.."
 
 if test ! -z "$YARN_CACHE_FOLDER" ; then
+
+    if git log --format=%B HEAD | grep -q '[clear cache]' ; then
+        >&2 echo "Clear cache ${YARN_CACHE_FOLDER}..."
+        test -d "$YARN_CACHE_FOLDER" && rm -r "$YARN_CACHE_FOLDER"
+    fi
+
     mkdir -p "$YARN_CACHE_FOLDER"
     echo "Yarn cache ($YARN_CACHE_FOLDER): $(ls -l "$YARN_CACHE_FOLDER" | wc -l)"
 fi
