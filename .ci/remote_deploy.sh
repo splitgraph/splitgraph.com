1d4188b9a12d711c916e5f84a85599a8c023f242
Miles Richardson <milesrichardson@gmail.com>
2020-04-30 00:03:41 +0000

    Remove deprecated CI scripts

--- .ci/remote_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -xe
-
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
-# some wild security by obscurity going on here
-rsync -ahIe 'ssh -p 9092' -r website/build/splitgraph/ travis@splitgraph.com:/var/www/sg/   # add --delete to remove everything existing
