9a28b126b17d4257c642da72f3d4c2a719a128bb
Miles Richardson <milesrichardson@gmail.com>
2020-04-30 23:44:18 +0000

    Deploy preview improvements: robots.txt, working(?) preview URL
    - Seems like shell redirects don't work in actions yml, so
        make the output file `preview_url.txt` an argument to `get_preview_url.sh`
    - Inject `robots.txt` into the bundle that disallows all, so that
        we don't accidentally get google indexing preview URLs
    - Add `preview_url.txt` to `.gitignore`

--- .ci/deploy_preview.sh
+++ .ci/deploy_preview.sh
@@ -6,7 +6,19 @@ SPLITGRAPH_DIR="$CI_DIR/.."
 COMMITISH="$(git rev-parse HEAD | cut -c -8)"
 COMMIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
+# We don't want google indexing the preview website(s)
+# Inject robots.txt here instead of at build step, because this is only for preview
+inject_robots_txt() {
+    cat <<EOF > "${SPLITGRAPH_DIR}/docs/out/robots.txt" && return 0
+User-agent: *
+Disallow: /
+EOF
+    return 1
+}
+
 pushd "$SPLITGRAPH_DIR" \
+    && inject_robots_txt \
+    && echo "Deploy preview: $COMMITISH ($COMMIT_BRANCH)" \
     && yarn deploy \
         $( test ! -z $NOW_TOKEN && echo "--token $NOW_TOKEN" ) \
         --force \
--- .ci/get_preview_url.sh
+++ .ci/get_preview_url.sh
@@ -2,16 +2,28 @@
 
 # Get the preview URL for the latest deployment with this commitish as metadata
 # Print the preview URL to stdout for capture, and also to stderr for debugging
-# Usage example:
+#
+# Usage example to capture to stdout:
 #   preview_url="$(./.ci/get_preview_url.sh)"
+#
+# or, to save to file `preview_url.txt`:
+#   ./.ci/get_preview_url.sh preview_url.txt
+#
+# or, to save to file `preview_url.txt` using commitish `f8f3f20e`
+#   ./ci/get_preview_url.sh preview_url.txt f8f3f20e
+#
+# or, to save to no file (/dev/null) using commitish `f8f3f20e`
+#   ./ci/get_preview_url.sh /dev/null f8f3f20e
+#
 
 CI_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 SPLITGRAPH_DIR="$CI_DIR/.."
 
-COMMITISH="$(git rev-parse HEAD | cut -c -8)"
+OUTPUT_FILE="${1-/dev/null}"
+COMMITISH=${2-"$(git rev-parse HEAD | cut -c -8)"}
 
 cd "$SPLITGRAPH_DIR" \
-    && >&2 echo -n "Getting preview URL... " \
+    && >&2 echo -n "Getting preview URL for $COMMITISH (write to $OUTPUT_FILE)... " \
     && ( yarn run deploy ls splitgraph-web \
         -m COMMITISH="$COMMITISH" \
         2> /dev/null \
@@ -19,7 +31,8 @@ cd "$SPLITGRAPH_DIR" \
         | awk '{print $2}' \
         | head -n1 \
         | tr -d '\n' \
-        | awk '{print;print > "/dev/stderr"}' ) \
+        | awk '{print;print > "/dev/stderr"}' \
+        | tee "$OUTPUT_FILE" ) \
     && exit 0
 
 >&2 echo "Fatal Error: Could not get preview URL"
