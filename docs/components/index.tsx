42eae5297becb7465ffd3a1102863aed1744f094
Miles Richardson <milesrichardson@gmail.com>
2020-05-06 01:12:34 +0000

    Add FeatureSection (stub component, todo)

--- docs/components/index.tsx
+++ docs/components/index.tsx
@@ -1,3 +1,4 @@
 export { LandingPageLayout } from "./LandingPageLayout";
-export { HeroBox } from './HeroBox';
+export { HeroBox } from "./HeroBox";
+export { FeatureSection } from './FeatureSection';
 
