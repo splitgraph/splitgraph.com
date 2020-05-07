854f3dc63d58dce2e25de4bc17871cdd7637ce7e
Miles Richardson <milesrichardson@gmail.com>
2020-05-07 16:10:28 +0000

    Cleanup styling of interpage (next/prev) links in docs

--- design/Layout/Containers/ContentFooter.js
+++ design/Layout/Containers/ContentFooter.js
@@ -1,22 +1,22 @@
 import { Box } from "../../index";
 
-const getStyle = depth => ({
+const getStyle = (depth) => ({
   paddingLeft: 4,
   paddingRight: 4,
   paddingBottom: 4,
   "@media (min-width: 769px)": {
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
   "@media (max-width: 768px)": {
-    backgroundColor: "primary",
-    color: "white",
-    borderRadius: "40px",
+    // backgroundColor: "primary",
+    // color: "white",
+    // borderRadius: "40px",
     paddingTop: `${4 * (depth - 1)}rem`,
     display: "flex",
     alignItems: "flex-end",
     justifyContent: "center",
-    boxShadow: "0 0 4px rgba(0, 0, 0, .125)"
-  }
+    // boxShadow: "0 0 4px rgba(0, 0, 0, .125)",
+  },
 });
 
 export default ({ children, depth }) => {
