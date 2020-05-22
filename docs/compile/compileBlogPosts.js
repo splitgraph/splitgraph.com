2ba8a5f487a734829de59d6ebadf243a7fc3f0a7
Miles Richardson <milesrichardson@gmail.com>
2020-05-22 17:43:46 +0000

    Add ability to sort children of ContentTree, and sort blog posts by most recent first
    - Add `sortTree` method which leverages `mapTree` to sort each node's children
    - Allow sorting `ContentTree` at compile time
    - When compiling blog posts, sort by `metadata.date`, most recent date first
    - (Also: update metadata regex to allow for `+` characters in JSON)

--- docs/compile/compileBlogPosts.js
+++ docs/compile/compileBlogPosts.js
@@ -1,12 +1,17 @@
 const path = require("path");
 const compileContentTree = require("./compileContentTree");
+const dayjs = require("dayjs");
 
 const CONTENT_DIR = path.dirname(require.resolve("@splitgraph/content"));
 const DOCS_DIR = `${path.join(CONTENT_DIR, "blog")}`;
 
+const mostRecentFirst = (a, b) =>
+  dayjs(a.metadata.date).isBefore(dayjs(b.metadata.date)) ? 1 : -1;
+
 const blogPosts = compileContentTree({
   contentDir: DOCS_DIR,
   urlPrefix: "/blog",
+  sort: mostRecentFirst,
 });
 
 module.exports = blogPosts;
