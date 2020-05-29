e024e1edb995d4c952684aebcec8c748d5491ce9
Miles Richardson <milesrichardson@gmail.com>
2020-05-29 04:32:36 +0000

    Set canonical URL of versioned docs pages to the latest URL
    - In order to avoid duplicate content penalties because of duplicate pages
       (e.g. every python API or sgr CLI page also exists under its version), set the
       canonical URL of every "versioned" page to the latest (e.g., the URL
       without the `/versions/v0.1.0/`) portion included
    
    ```
    // e.g.:
    // /docs/python-api/versions/v0.1.0/splitgraph.engine.postgres
    //                               ^____delete___^
    // /docs/sgr/versions/v0.1.0/image-management-creation/checkout
    //                  ^____delete____^
    ```
    
    - Add `getSEO` option to `withHolyGrailLayout` so `withDocsLayout` can use
      custom logic to implement this routine

--- templaters/layouts/withDocsLayout.js
+++ templaters/layouts/withDocsLayout.js
@@ -3,6 +3,31 @@ import { jsx } from "theme-ui";
 import React from "react";
 import withHolyGrailLayout from "./withHolyGrailLayout";
 
+const getDocsSEO = ({ currentURL, meta, SEO_BASE_URL }) => {
+  const isVersionedDocument = currentURL.includes("/versions/");
+
+  if (!isVersionedDocument) {
+    return {};
+  }
+
+  // e.g.:
+  // /docs/python-api/versions/v0.1.0/splitgraph.engine.postgres
+  //                  ^____delete___^
+  // /docs/sgr/versions/v0.1.0/image-management-creation/checkout
+  //          ^____delete____^
+  const splitURL = currentURL.split("/");
+  const versionsIndex = splitURL.findIndex((x) => x === "versions");
+  splitURL.splice(versionsIndex, 2);
+  const canonicalURL = splitURL.join("/");
+
+  const injectSEO = {
+    canonical: `${SEO_BASE_URL}${canonicalURL}`,
+  };
+
+  return injectSEO;
+};
+
 export default withHolyGrailLayout({
   titleTemplate: "%s - Documentation | Splitgraph",
+  getSEO: getDocsSEO,
 });
