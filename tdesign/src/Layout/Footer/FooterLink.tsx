4c2f7f06b89d8d2d751053d54432fdca896d6cee
Miles Richardson <milesrichardson@gmail.com>
2020-06-04 01:44:28 +0000

    Make `Link` optional prop in `<Footer>` to fix weird catalog bug
    - Weird build error was happening in catalog when passing `Link`
       component as a prop, probably due to some transpilation weirdness
       and how it interacts with apollo SSR
    - Sidestep the issue by making the `Link` prop optional in `<Footer>`
    - When no `Link` prop exists, use `<a>` (same behavior as before, but
       now the catalog does not explicitly pass down an `<a>` wrapper component)
    - Docs site still passes `Link` component (which works fine because no apollo SSR?)

--- tdesign/src/Layout/Footer/FooterLink.tsx
+++ tdesign/src/Layout/Footer/FooterLink.tsx
@@ -4,13 +4,25 @@ import { jsx } from "theme-ui";
 import * as React from "react";
 
 export interface IFooterLinkProps {
-  Link: React.FunctionComponent<any>;
+  Link?: React.FunctionComponent<any>;
   href: string;
   children: React.ReactNode;
 }
 
 const FooterLink = ({ Link, href, children }: IFooterLinkProps) => {
-  return <Link href={href}>{children}</Link>;
+  const HTMLLink = ({
+    children,
+    href,
+  }: {
+    children: React.ReactNode;
+    href: string;
+  }) => <a href={href}>{children}</a>;
+
+  return !!Link ? (
+    <Link href={href}>{children}</Link>
+  ) : (
+    <HTMLLink href={href}>{children}</HTMLLink>
+  );
 };
 
 export default FooterLink;
