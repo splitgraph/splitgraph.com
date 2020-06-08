1bf4ce09c3d699bb97b959926e6d4b630b7379c4
Miles Richardson <milesrichardson@gmail.com>
2020-06-08 15:18:16 +0000

    Style tweaks
    - Set fixed height of LogoImage so that when it's uncached and loading,
       the page does not appear to "jump" up and down between clicks.
       In practice this would only be a problem on first page load and in dev,
       since the logo should be cached in prod, but it was annoying
    - Support injecting extraStyle prop to `ContentHeader`

--- tdesign/src/Layout/ContentHeader/ContentHeader.tsx
+++ tdesign/src/Layout/ContentHeader/ContentHeader.tsx
@@ -1,29 +1,34 @@
 // @jsx jsx
 // @ts-ignore
-import { jsx, Box, SystemStyleObject } from 'theme-ui';
-import * as React from 'react';
+import { jsx, Box, SystemStyleObject } from "theme-ui";
+import * as React from "react";
 
 export interface IContentHeaderProps {
   children?: React.ReactNode;
+  extraStyle?: SystemStyleObject;
 }
 
-const headerContainerStyle = {
-  marginBottom: '2rem',
-  color: 'primary',
-  '.content-header--banner': {
-    padding: '0.5em',
-    marginBottom: '2rem',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignContent: 'center',
-    h2: {
-      display: 'inline',
-      margin: 0,
+export default ({ children, extraStyle = {} }: IContentHeaderProps) => {
+  const headerContainerStyle = {
+    marginBottom: "2rem",
+    color: "primary",
+    ...extraStyle,
+    ".content-header--banner": {
+      padding: "0.5em",
+      marginBottom: "2rem",
+      display: "flex",
+      justifyContent: "space-between",
+      alignContent: "center",
+      ...(extraStyle.hasOwnProperty(".content-header--banner")
+        ? extraStyle[".content-header--banner"]
+        : {}),
+      h2: {
+        display: "inline",
+        margin: 0,
+      },
     },
-  },
-} as SystemStyleObject;
+  } as SystemStyleObject;
 
-export default ({ children }: IContentHeaderProps) => {
   return (
     <Box sx={headerContainerStyle}>
       <Box className="content-header--banner">{children}</Box>
--- tdesign/src/Layout/LogoImage/LogoImage.tsx
+++ tdesign/src/Layout/LogoImage/LogoImage.tsx
@@ -1,18 +1,20 @@
 // @jsx jsx
 // @ts-ignore
-import { jsx } from 'theme-ui';
-import * as React from 'react';
+import { jsx } from "theme-ui";
+import * as React from "react";
 
 export interface ILogoImageProps {
   logoURL?: string;
 }
 
 const logoImageStyle = {
-  maxHeight: '2rem',
+  minHeight: "2rem",
+  height: "2rem",
+  maxHeight: "2rem",
   marginLeft: 4,
 };
 
-const defaultLogoURL = '/static/splitgraph_logo.svg';
+const defaultLogoURL = "/static/splitgraph_logo.svg";
 
 export default ({ logoURL = defaultLogoURL }: ILogoImageProps) => {
   return <img src={logoURL} style={logoImageStyle} alt="splitgraph logo" />;
