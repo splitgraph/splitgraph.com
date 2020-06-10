bbcd848f86f53bfa7edf23c0d689a111201081ee
Miles Richardson <milesrichardson@gmail.com>
2020-06-10 15:45:01 +0000

    Change `<Link>` props to account for dynamic query params
    - Allow `as` and `href` props to be an object with `pathname` and
       `query` in addition to `string` or `undefined`

--- tdesign/src/Link/Link.tsx
+++ tdesign/src/Link/Link.tsx
@@ -1,19 +1,27 @@
 // @jsx jsx
-import { Styled } from 'theme-ui';
-import * as React from 'react';
-import Link from 'next/link';
+import { Styled, SystemStyleObject } from "theme-ui";
+import * as React from "react";
+import Link from "next/link";
+
+export interface INextDynamicLinkProps {
+  pathname: string;
+  query: { [key: string]: string };
+}
 
 export interface LinkProps
   extends React.HTMLAttributes<
     HTMLAnchorElement | HTMLButtonElement | HTMLElement
   > {
-  href: string;
-  as?: string;
+  href: string | INextDynamicLinkProps;
+  as?: string | INextDynamicLinkProps;
   replace?: boolean;
   variant?: string;
+
+  // note: sx is deprecated, but keep around in case anything (wrongly) using it
   sx?: any;
+  extraStyle?: SystemStyleObject;
   children: React.ReactNode;
-  target?: '_blank' | '_self' | '_parent' | '_top' | string;
+  target?: "_blank" | "_self" | "_parent" | "_top" | string;
 }
 
 export default React.forwardRef(
@@ -22,15 +30,16 @@ export default React.forwardRef(
       href,
       as,
       replace,
-      variant = 'links.primary',
+      variant = "links.primary",
       sx = {},
+      extraStyle = {},
       children,
       ...rest
     }: LinkProps,
     ref: any
   ) => (
     <Link href={href} as={as} passHref>
-      <Styled.a sx={{ variant, ...sx }} ref={ref} {...rest}>
+      <Styled.a sx={{ variant, ...sx, ...extraStyle }} ref={ref} {...rest}>
         {children}
       </Styled.a>
     </Link>
