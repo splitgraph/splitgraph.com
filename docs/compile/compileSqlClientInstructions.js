5ae3ddc87034ddebffadf56a21fcd6397693a234
Miles Richardson <milesrichardson@gmail.com>
2020-08-13 17:49:11 +0000

    Add .mdx file for each BI client, and test new dynamic import technique
    - Trying a new dynamic import technique, not sure
       if it will build in CI or not. Works in dev

---
+++ docs/compile/compileSqlClientInstructions.js
@@ -0,0 +1,23 @@
+const glob = require("glob");
+const path = require("path");
+
+const BASE_DIR = "@splitgraph/content/sql-client-instructions";
+
+const mdxDirIdx = require.resolve(BASE_DIR);
+const mdxDir = path.dirname(mdxDirIdx);
+const fileWildcard = `${mdxDir}/*.mdx`;
+
+const components = [];
+
+glob.sync(fileWildcard).forEach(function(file) {
+  const ComponentName = path.basename(file).replace(".mdx", "");
+
+  components.push({
+    ComponentName,
+  });
+});
+
+module.exports = {
+  BASE_DIR,
+  components,
+};
