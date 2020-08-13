d021ca05369999f852267312587f2cd087d136e0
Miles Richardson <milesrichardson@gmail.com>
2020-08-13 15:55:20 +0000

    Move OAuth buttons, PreWithCopy from cloud/catalog into public repo
    - Need this code for the connection info hero section
    
    (todo: sync up both sides)

---
+++ tdesign/src/OAuthButton/index.tsx
@@ -0,0 +1,11 @@
+import GitHubOAuthButton from "./GitHubOAuthButton";
+export { GitHubOAuthButton };
+
+import GitLabOAuthButton from "./GitLabOAuthButton";
+export { GitLabOAuthButton };
+
+import GoogleOAuthButton from "./GoogleOAuthButton";
+export { GoogleOAuthButton };
+
+import type { IOAuthButtonProps } from "./OAuthButton"
+export type { IOAuthButtonProps }
