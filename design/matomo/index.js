369b15586b399a8692f5cd61be12a424986b1d92
Miles Richardson <milesrichardson@gmail.com>
2020-08-19 10:15:13 +0000

    Telemetry / analytics improvements
    - Allow sending events to matomo
    - If access token exists and user_id is set, set it in matomo
    - Turn off logging in ifame resizer

--- design/matomo/index.js
+++ design/matomo/index.js
@@ -16,10 +16,15 @@ export function matomoInit({
   if (navigator.sendBeacon) {
     var origSendBeacon = navigator.sendBeacon;
     navigator.sendBeacon = function() {
+      const cleanSearchParams = (origSearch) => {
+        // We need to remove the leading `?` from the original search params,
+        // since lua will insert it at the other end, and a double ?? makes Big Sad
+        return `?q=${btoa(origSearch.substring(1))}`;
+      };
+
       if (typeof arguments[0] === "string") {
         var _url = new URL(arguments[0]);
-
-        _url.search = `?q=${btoa(_url.search)}`;
+        _url.search = `${cleanSearchParams(_url.search)}`;
         arguments[0] = _url.toString();
       }
 
@@ -49,6 +54,32 @@ export function matomoInit({
   maybePush(["setTrackerUrl", `${url}/${phpTrackerFile}`]);
   maybePush(["setSiteId", siteId]);
 
+  // Maybe get the access token
+  if (
+    document &&
+    document.cookie &&
+    document.cookie.includes("sgr.token.access")
+  ) {
+    try {
+      const userId = JSON.parse(
+        atob(
+          document.cookie
+            .split("; sgr.token.access=")
+            .pop()
+            .split(";")
+            .shift()
+            .split(".")[1]
+        )
+      ).user_id;
+
+      if (userId) {
+        matomoSetUserId({ userId });
+      }
+    } catch (e) {
+      console.warn("Error trying to parse user_id from access token:", e);
+    }
+  }
+
   // Use location.pathname on initial load, then Router.pathname on subsequent CSR requests
   const scriptElement = document.createElement("script");
   const refElement = document.getElementsByTagName("script")[0];
@@ -75,6 +106,32 @@ export function matomoInit({
       previousPath = pathname;
     }, 0);
   });
+}
+
+export function matomoSetUserId({ userId }) {
+  if (!window) {
+    return;
+  }
+
+  if (!userId) {
+    console.warn("Cannot set userId to non-existing ID");
+    return;
+  }
+
+  matomoPush(["setUserId", userId]);
+}
+
+export function matomoTrack({ category, action, name, value }) {
+  if (!window) {
+    return;
+  }
+
+  if (!category || !action || !name) {
+    console.warn("trackEvent missing category, action or name. Not pushing.");
+    return;
+  }
+
+  matomoPush(["trackEvent", category, action, name, ...(value ? [value] : [])]);
 }
 
 export function matomoPush(args) {
