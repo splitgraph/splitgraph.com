96a0dacaebe45f554fc98714ef58ba7b91ab4b6b
Miles Richardson <milesrichardson@gmail.com>
2021-03-30 04:55:29 +0000

    Add a separate `tsconfig.ci.json` for running in CI
    - Our default `tsconfig.json` extends from `../`, but when running in the
       context of public GitHub, no such directory exists
    - So, make a new root `tsconfig.ci.json` file, and at CI time, overwrite
       `tsconfig.json` with it

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,6 +4,7 @@ CI_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 SPLITGRAPH_DIR="$CI_DIR/.."
 
 pushd "$SPLITGRAPH_DIR" \
+    && mv tsconfig.ci.json tsconfig.json \
     && yarn build \
     && cp "$SPLITGRAPH_DIR"/docs/proxyDirectories.txt "$SPLITGRAPH_DIR"/docs/out/proxyDirectories.txt \
     && echo "Build successful" \
