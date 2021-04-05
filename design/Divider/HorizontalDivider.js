e8845ccb15c0be2dc87a268941f58a36d0be03aa
Patrick Skinner <pas8314@gmail.com>
2021-04-05 23:04:39 +0000

    Clean up last remant of non TS <Input> Annotate HorizontalDivider, which appears to be unused

--- design/Divider/HorizontalDivider.js
+++ design/Divider/HorizontalDivider.js
@@ -1,6 +1,8 @@
 /** @jsxImportSource theme-ui */
 import { Box } from "theme-ui";
 
+/* NOTE 20210406: this file appears to not be used? */
+
 const HorizontalDivider = ({ width = 9 / 10, sx = {}, children, ...rest }) => (
   <Box sx={{ display: "flex", justifyContent: "center" }}>
     <Box
