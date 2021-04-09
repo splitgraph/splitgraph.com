bab27d2e15ba5a6b72599fb010a82ae0a8beebbc
Patrick Skinner <pas8314@gmail.com>
2021-04-09 11:36:36 +0000

    MuiLink lives in splitgraph.com
    - consistent with other <Link>/<MutedLink> components

--- tdesign/src/Link/index.tsx
+++ tdesign/src/Link/index.tsx
@@ -2,6 +2,7 @@ import Link from "./Link";
 import type { INextDynamicLinkProps, LinkProps } from "./Link";
 import MutedLink from "./MutedLink";
 import RouterMock from "./RouterMock";
+import MuiLink, { NextLinkComposed } from "./MuiLink";
 
 export type { INextDynamicLinkProps, LinkProps };
-export { Link, MutedLink, RouterMock };
+export { Link, MutedLink, RouterMock, MuiLink, NextLinkComposed };
