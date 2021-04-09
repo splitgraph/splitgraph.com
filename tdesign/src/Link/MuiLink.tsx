59cb2bebc7604f5b3fa46b32b0bfa462b0448d68
Patrick Skinner <pas8314@gmail.com>
2021-04-09 11:36:36 +0000

    MuiLink lives in splitgraph.com
    - consistent with other <Link>/<MutedLink> components

---
+++ tdesign/src/Link/MuiLink.tsx
@@ -0,0 +1,120 @@
+import { forwardRef, AnchorHTMLAttributes } from "react";
+import clsx from "clsx";
+import { useRouter } from "next/router";
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import MuiLink, { LinkProps as MuiLinkProps } from "@material-ui/core/Link";
+interface NextLinkComposedProps
+  extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href">,
+    Omit<NextLinkProps, "href"> {
+  to: NextLinkProps["href"];
+  href?: NextLinkProps["href"];
+}
+
+export const NextLinkComposed = forwardRef<
+  HTMLAnchorElement,
+  NextLinkComposedProps
+>(function NextLinkComposed(props, ref) {
+  const {
+    to,
+    href,
+    replace,
+    scroll,
+    passHref,
+    shallow,
+    prefetch,
+    locale,
+    ...other
+  } = props;
+
+  return (
+    <NextLink
+      href={to}
+      prefetch={prefetch}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref={passHref}
+      locale={locale}
+    >
+      <a ref={ref} {...(other as any)} />
+    </NextLink>
+  );
+});
+
+export type LinkProps = {
+  activeClassName?: string;
+  href: NextLinkProps["href"];
+  noLinkStyle?: boolean;
+} & Omit<NextLinkComposedProps, "to" | "href"> &
+  Omit<MuiLinkProps, "href">;
+
+// A styled version of the Next.js Link component:
+// https://nextjs.org/docs/#with-link
+const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link(
+  props,
+  ref
+) {
+  const {
+    activeClassName = "active",
+    className: classNameProps,
+    href,
+    noLinkStyle,
+    role, // Link don't have roles.
+    ...other
+  } = props;
+
+  const router = useRouter();
+  const pathname = typeof href === "string" ? href : href.pathname;
+  const className = clsx(classNameProps, {
+    [activeClassName]: router.pathname === pathname && activeClassName,
+  });
+
+  const isExternal =
+    typeof href === "string" &&
+    (href.indexOf("http") === 0 || href.indexOf("mailto:") === 0);
+
+  if (isExternal) {
+    if (noLinkStyle) {
+      return (
+        <a
+          className={className}
+          href={href as string}
+          ref={ref as any}
+          {...(other as any)}
+        />
+      );
+    }
+
+    return (
+      <MuiLink
+        className={className}
+        href={href as string}
+        ref={ref}
+        {...(other as any)}
+      />
+    );
+  }
+
+  if (noLinkStyle) {
+    return (
+      <NextLinkComposed
+        className={className}
+        ref={ref as any}
+        to={href}
+        {...(other as any)}
+      />
+    );
+  }
+
+  return (
+    <MuiLink
+      component={NextLinkComposed}
+      className={className}
+      ref={ref}
+      to={href}
+      {...(other as any)}
+    />
+  );
+});
+
+export default Link;
