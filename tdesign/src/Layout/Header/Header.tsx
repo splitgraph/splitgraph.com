8ada57afa21d22bd6c3bec66b9a97caf16bdcbaa
Patrick Skinner <pas8314@gmail.com>
2021-04-15 02:29:15 +0000

    WIP <Header>/<HeaderLeft> better matches comp

--- tdesign/src/Layout/Header/Header.tsx
+++ tdesign/src/Layout/Header/Header.tsx
@@ -3,74 +3,79 @@
 import * as React from "react";
 
 import { Box } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 
 export interface HeaderProps {
   children?: React.ReactNode;
   style?: any;
 }
 
-const headerSx: any = {
-  backgroundColor: "heavy",
-  // borderBottomColor: "primary",
-  // borderBottomStyle: "solid",
-  // borderBottomWidth: "1px",
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 2fr",
-  gridTemplateRows: "1fr",
-  gridColumnGap: "0px",
-  gridRowGap: "0px",
-  minHeight: "5vh",
-  padding: "1em",
-  ".header--left": {
-    gridArea: "1 / 1 / 2 / 2",
-    backgroundColor: "heavy",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  ".header--center": {
-    gridArea: "1 / 2 / 2 / 3",
-    // backgroundColor: 'red',
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    color: "light",
-  },
-  ".header--right": {
-    gridArea: "1 / 3 / 2 / 4",
-    // backgroundColor: 'purple',
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-  },
-  ".logo-text": {
-    color: "sglightblue",
-    // fontSize: '1.5rem',
-    paddingLeft: "1ch",
-    ":hover": {
-      color: "white",
-    },
-  },
-  a: {
-    color: "light",
-    fontWeight: "bold",
-    textDecoration: "underline",
-    marginRight: "1ch",
-  },
-  "a:hover": {
-    borderBottomStyle: "solid",
-    borderBottomWidth: "1px",
-    borderBottomColor: "light",
-  },
-  ".button-link": {
-    variant: "links.button",
-  },
+const Header = ({ children }: HeaderProps) => {
+  const theme = useTheme();
+  return (
+    <Box
+      className="header--container"
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 2fr",
+        gridTemplateRows: "1fr",
+        gridColumnGap: "0px",
+        gridRowGap: "0px",
+        minHeight: "5vh",
+        // display: { xs: "none", md: "flex" },
+        // padding: { xs: ".3em", md: "1em" },
+        padding: theme.spacing(1),
+        [theme.breakpoints.down("md")]: {
+          padding: theme.spacing(0.3),
+        },
+        ".header--left": {
+          gridArea: "1 / 1 / 2 / 2",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        },
+        ".header--center": {
+          gridArea: "1 / 2 / 2 / 3",
+          // backgroundColor: 'red',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          // color: "light",
+        },
+        ".header--right": {
+          gridArea: "1 / 3 / 2 / 4",
+          // backgroundColor: 'purple',
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        },
+        ".logo-text": {
+          // color: "sglightblue",
+          // fontSize: '1.5rem',
+          paddingLeft: "1ch",
+          ":hover": {
+            color: "white",
+          },
+        },
+        a: {
+          // color: "light",
+          fontWeight: "bold",
+          textDecoration: "underline",
+          marginRight: "1ch",
+        },
+        "a:hover": {
+          borderBottomStyle: "solid",
+          borderBottomWidth: "1px",
+          borderBottomColor: "light",
+        },
+        ".button-link": {
+          variant: "links.button",
+        },
+      }}
+    >
+      {children}
+    </Box>
+  );
 };
-
-const Header = ({ children }: HeaderProps) => (
-  <Box className="header--container" sx={headerSx}>
-    {children}
-  </Box>
-);
 
 export default Header;
