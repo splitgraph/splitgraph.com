f9c1434c0cf6231952c17a7bae9a77b7fa29c495
Patrick Skinner <pas8314@gmail.com>
2021-04-28 14:15:02 +0000

    Tentative Load More button from comp
    - Uses colors as defined in MUI theme

---
+++ tdesign/src/Button/LoadMoreButton.tsx
@@ -0,0 +1,29 @@
+import { Button, Typography } from "@material-ui/core";
+import { IconArrowDown } from "@splitgraph/tdesign";
+
+export interface LoadMoreButtonProps {}
+
+const LoadMoreButton = ({ children, ...rest }) => {
+  return (
+    <Button
+      variant="pill"
+      sx={{
+        border: (theme) => `1px solid ${theme.palette.flambeeRed.light}`,
+        padding: "12px",
+        boxShadow: "none",
+      }}
+      {...rest}
+    >
+      <IconArrowDown sx={{ mr: "9px", height: "8px" }} />
+      <Typography
+        variant="bodyHighlighted"
+        color="flambeeRed.main"
+        sx={{ mr: "20px" }}
+      >
+        {children}
+      </Typography>
+    </Button>
+  );
+};
+
+export default LoadMoreButton;
