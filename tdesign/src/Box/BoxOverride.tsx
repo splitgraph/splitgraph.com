26f592631e3a910045e45e7404ff23c5863a4675
Patrick Skinner <pas8314@gmail.com>
2021-04-28 14:12:10 +0000

    BoxOverride - support rest of the variants

--- tdesign/src/Box/BoxOverride.tsx
+++ tdesign/src/Box/BoxOverride.tsx
@@ -15,11 +15,43 @@ const paddedContentAreaStyle = {
   paddingLeft: ["2rem", "2rem", "10%"],
   paddingRight: ["2rem", "2rem", "10%"],
 };
+const errorNoticeStyle = {
+  display: "flex",
+  alignItems: "center",
+  '[class^="sg-icon-"]': {
+    display: "inline-block",
+    marginRight: "1em",
+  },
+  borderWidth: "1px",
+  borderStyle: "solid",
+  padding: ".5em",
+  a: {
+    variant: "links.primary",
+  },
+  backgroundColor: (theme) => theme.palette.errorBackground.main,
+  borderColor: (theme) => theme.palette.danger.main,
+};
+const errorTextStyle = {
+  color: (theme) => theme.palette.danger.main,
+};
+const basicWhiteStyle = {
+  borderWidth: "1px",
+  borderStyle: "solid",
+  borderColor: (theme) => theme.palette.primary.main,
+  backgroundColor: "white",
+  padding: "1rem",
+};
 
 const BoxOverride = (props: BoxOverrideProps) => {
   const { variant, sx, ...rest } = props;
   if (variant === "paddedContentArea") {
     return <Box sx={{ ...sx, ...paddedContentAreaStyle }} {...rest} />;
+  } else if (variant === "errorNotice") {
+    return <Box sx={{ ...sx, ...errorNoticeStyle }} {...rest} />;
+  } else if (variant === "errorText") {
+    return <Box sx={{ ...sx, ...errorTextStyle }} {...rest} />;
+  } else if (variant === "basicWhite") {
+    return <Box sx={{ ...sx, ...basicWhiteStyle }} {...rest} />;
   }
   return <Box {...rest} />;
 };
