56d17f6c49bb98a58b883b1664c0d6dc17e83279
Patrick Skinner <pas8314@gmail.com>
2021-04-29 21:42:54 +0000

    Types + getContrastText example

--- tdesign/src/Text/SuccessText.tsx
+++ tdesign/src/Text/SuccessText.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@material-ui/core";
 const SuccessText = ({ children, sx = {}, ...rest }) => (
   <Typography
     sx={{
-      color: "#3B8D36", //TODO use MUI theme
+      color: "success.main",
       ...sx,
     }}
     className="success-text"
