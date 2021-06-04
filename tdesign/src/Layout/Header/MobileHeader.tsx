3d7562a43043e35b0e8c40427bd71a8df9600332
Patrick Skinner <pas8314@gmail.com>
2021-06-04 23:40:52 +0000

    MobileHeader

---
+++ tdesign/src/Layout/Header/MobileHeader.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Box, IconButton } from "@material-ui/core";
+import { Menu as MenuIcon } from "@material-ui/icons";
+import { LogoImage, LogoText } from "@splitgraph/tdesign/";
+
+interface IMobileHeaderProps {
+  logoText: string;
+  searchInput: React.ReactElement;
+}
+
+const MobileHeader = ({ logoText, searchInput }: IMobileHeaderProps) => {
+  return (
+    <Box sx={{ p: "12px" }}>
+      <Box
+        sx={{ display: "flex", justifyContent: "space-between", px: "20px" }}
+      >
+        <a aria-label="home" href={"/"}>
+          <LogoImage logoURL={"/static/brandmark.svg"} />
+          <LogoText text={logoText} />
+        </a>
+        <IconButton onClick={() => {}}>
+          <MenuIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{ width: "100%" }}>
+        {React.cloneElement(searchInput, {
+          fullWidth: true,
+        })}
+      </Box>
+    </Box>
+  );
+};
+
+export default MobileHeader;
--- tdesign/src/Layout/Header/index.tsx
+++ tdesign/src/Layout/Header/index.tsx
@@ -1,11 +1,7 @@
 import Header from "./Header";
-
-export { Header };
 import HeaderLeft from "./HeaderLeft";
-export { HeaderLeft };
-
 import HeaderCenter from "./HeaderCenter";
-export { HeaderCenter };
-
 import HeaderRight from "./HeaderRight";
-export { HeaderRight };
+import MobileHeader from "./MobileHeader";
+
+export { Header, HeaderLeft, HeaderCenter, HeaderRight, MobileHeader };
