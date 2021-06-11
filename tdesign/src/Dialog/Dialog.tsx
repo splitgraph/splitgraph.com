1c3381205722ae6a0d3a9f3ec3fb2bc6c94d4c9a
Patrick Skinner <pas8314@gmail.com>
2021-06-11 22:16:37 +0000

    Migrate Dialog from catalog to splitgraph.com
    - .com is probably a better home for it since it's re-usable

---
+++ tdesign/src/Dialog/Dialog.tsx
@@ -0,0 +1,81 @@
+import {
+  Dialog as MuiDialog,
+  DialogProps,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+
+interface IDialogProps extends DialogProps {
+  open: boolean;
+  title?: string;
+  setShowDialog: (T: boolean) => void;
+  children: React.ReactNode;
+}
+
+const Dialog = ({ open, title, setShowDialog, children }: IDialogProps) => {
+  return (
+    <MuiDialog
+      open={open}
+      fullScreen
+      aria-labelledby={title}
+      sx={{
+        ".MuiDialogContent-root": { py: 0 },
+      }}
+    >
+      <CloseableDialogTitle
+        id={title}
+        onClose={() => {
+          setShowDialog(false);
+          console.log("closed MobileDialog");
+        }}
+      >
+        {title}
+      </CloseableDialogTitle>
+      <DialogContent sx={{ justifyContent: "flex-end" }}>
+        {children}
+      </DialogContent>
+    </MuiDialog>
+  );
+};
+export default Dialog;
+
+export interface ICloseableDialogTitleProps {
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
+}
+const CloseableDialogTitle = (props: ICloseableDialogTitleProps) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle
+      disableTypography
+      sx={{
+        m: 0,
+        p: 2,
+      }}
+      {...other}
+    >
+      <Typography variant="h6" component="div">
+        {children}
+      </Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.flambeeDarkGray.dark,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
