7305fe58ec61a04eefcd87a00a07973b9f15af28
Patrick Skinner <pas8314@gmail.com>
2021-07-01 21:47:23 +0000

    New designer colors of June 2021 live in their own file
    - We want to make the colors available in MUI and Emotion, without tying ourselves to one or the other.
    - Next up: defaultTheme.ts references colors from here
    - Still a cpl Qs to answer, but should be a good starting point to feed into places that need color knowledge

---
+++ tdesign/src/themes/design.ts
@@ -0,0 +1,61 @@
+export const theme = {
+  // Grays //////////////////
+  grays: {
+    light: {
+      gray20: "#000202",
+      gray21: "#2C2D2D",
+      gray22: "#555656",
+      gray23: "#818285",
+      gray24: "#A0A3A9",
+      gray25: "#C0C3CC",
+      gray26: "#E6E7EB",
+      gray27: "#F2F4F6",
+      gray28: "#F9FAFB",
+      gray29: "#FCFCFD",
+    },
+    dark: {
+      gray20: "#FCFCFD",
+      gray21: "#E6E7EB",
+      gray22: "#A0A3A9",
+      gray23: "#818285",
+      gray24: "#555656",
+      gray25: "#292A2A",
+      gray26: "#1C1E1E",
+      gray27: "#141616",
+      gray28: "#0D0F0F",
+      gray29: "#000202",
+      // Gray25-29: should be dynamic "add primary color at 8%"
+    },
+  },
+  //Brand ///////////////////
+  primary: {
+    main: "#F94569",
+    tint: "#FF7C97",
+    shade: "#C0003F",
+  },
+  navbar: {
+    light: "#FBFCFF",
+    dark: "#201316",
+  },
+  footer: {
+    light: "#00224E",
+    dark: "201316",
+  },
+  // Surfaces ///////////////
+  surfaces: {
+    light: {
+      background: "#FFFFFF",
+      sql: "#E3EFFE",
+      error: "#F9EEEF",
+      success: "#DBF9F3",
+      link: "#2A81F6",
+    },
+    dark: {
+      background: "#000202", // should "add primary color at 8%"
+      sql: "#27293B",
+      error: "#370D10",
+      success: "#103D34",
+      link: "#2A81F6",
+    },
+  },
+};
