51cf50723ddc4165b6eaef03b783f36f96983efb
Patrick Skinner <pas8314@gmail.com>
2021-07-06 20:56:46 +0000

    Migrate styling demo to splitgraph.com

---
+++ docs/pages/docs/lp/arrow-text/ArrowText.style.tsx
@@ -0,0 +1,65 @@
+//@ts-nocheck
+import styled from "@emotion/styled";
+const rem = (pixel: number) => {
+  return `${pixel / 16}rem`;
+};
+
+const ArrowTextContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: ${rem(2)};
+  padding: ${rem(4)} ${rem(8)};
+  border: ${rem(1)} solid;
+
+  &.primary {
+    border-image: linear-gradient(
+        to right,
+        transparent,
+        rgba(249, 69, 105, 0.4)
+      )
+      1 stretch;
+
+    color: ${(props) => props.theme.palette.primary[100]};
+  }
+
+  &.secondary {
+    border-image: linear-gradient(
+        to right,
+        transparent,
+        rgba(83, 183, 166, 0.4)
+      )
+      1 stretch;
+
+    color: ${(props) => props.theme.palette.secondary[100]};
+  }
+
+  &.success {
+    border-image: linear-gradient(
+        to right,
+        transparent,
+        rgba(67, 118, 108, 0.4)
+      )
+      1 stretch;
+
+    color: ${(props) => props.theme.palette.success[300]};
+  }
+
+  h6 {
+    margin-right: ${rem(12)};
+  }
+
+  .icon {
+    transition: all 0.25s cubic-bezier(0.8, 0, 0.2, 1);
+  }
+
+  &:hover,
+  &:active {
+    .icon {
+      transform: translateX(${rem(4)});
+    }
+  }
+`;
+
+export default ArrowTextContainer;
