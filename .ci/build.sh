d0d38c3e87c613accd2e93264236598e176f80b1
Miles Richardson <milesrichardson@gmail.com>
2021-07-07 01:44:00 +0100

    Skip typechecking in `next build` (it doesn't use project references)
    - Do this the same as we do in the parent cloud repo
    - For "building" docs, suppress typechecking during `next build` compilation step
    - IIRC This is because Next.js doesn't support project references, and
      we are doing our own typechecking – since Babel will strip types during
      build anyway, we're unlikely to lose any functionality
    - Note that Next.js might report some typechecking errors that
    our TS structure will not, and by suppressing them, we might
    miss legitimate ones (I see one currently about Sx prop,
    but I'm not sure if it's helpful or a false positive - tbd)
    
    Note: It's possible this will actually pass the build step,
    but it's still a long and memory intensive build process -
    that's a separate issue.

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,7 +5,7 @@ SPLITGRAPH_DIR="$CI_DIR/.."
 
 pushd "$SPLITGRAPH_DIR" \
     && mv tsconfig.ci.json tsconfig.json \
-    && yarn build \
+    && yarn typecheck-and-build \
     && cp "$SPLITGRAPH_DIR"/docs/proxyDirectories.txt "$SPLITGRAPH_DIR"/docs/out/proxyDirectories.txt \
     && echo "Build successful" \
     && exit 0
