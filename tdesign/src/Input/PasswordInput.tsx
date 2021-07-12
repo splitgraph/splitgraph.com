56112f40424b27b9185eb861202516d0d0ccb7dc
Patrick Skinner <pas8314@gmail.com>
2021-07-12 12:14:17 +0000

    PasswordInput - support refs
    - Catalog uses refs to manage input values, this commit seeks to implement the needed changes to support that usage.
    - Should help us remain compat with previous ref-based forms in catalog, next up is react-hook-form support
    
    - CU-nn8rwq

--- tdesign/src/Input/PasswordInput.tsx
+++ tdesign/src/Input/PasswordInput.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import {
   Box,
   OutlinedInput,
+  OutlinedInputProps,
   InputAdornment,
   IconButton,
 } from "@material-ui/core/";
@@ -13,73 +14,80 @@ interface State {
   showPassword: boolean;
 }
 
-const PasswordInput = (props) => {
-  const [values, setValues] = useState<State>({
-    password: "",
-    showPassword: false,
-  });
-  const handleChange = (prop: keyof State) => (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
+const PasswordInput = forwardRef<HTMLInputElement, OutlinedInputProps>(
+  (props, ref) => {
+    const [values, setValues] = useState<State>({
+      password: "",
+      showPassword: false,
     });
-  };
+    // const handleChange = (prop: keyof State) => (
+    //   event: React.ChangeEvent<HTMLInputElement>
+    // ) => {
+    //   setValues({ ...values, [prop]: event.target.value });
+    // };
 
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
+    const handleClickShowPassword = () => {
+      setValues({
+        ...values,
+        showPassword: !values.showPassword,
+      });
+    };
 
-  return (
-    <Box
-      sx={{
-        "& .MuiOutlinedInput-root": {
-          "& fieldset": {
-            borderColor: theme.grays.light.gray26,
+    const handleMouseDownPassword = (
+      event: React.MouseEvent<HTMLButtonElement>
+    ) => {
+      event.preventDefault();
+    };
+
+    return (
+      <Box
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: theme.grays.light.gray26,
+            },
+            "&:hover fieldset": {
+              borderColor: "flambeeBlue.light",
+              boxShadow: "0px 4px 8px rgba(115, 176, 255, 0.15)",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "flambeeBlue.light",
+            },
           },
-          "&:hover fieldset": {
-            borderColor: "flambeeBlue.light",
-            boxShadow: "0px 4px 8px rgba(115, 176, 255, 0.15)",
+          "& .MuiOutlinedInput-root.Mui-error": {
+            backgroundColor: theme.surfaces.light.error,
+            "& fieldset": {
+              borderColor: "#B62B35", //TODO is this computed?
+            },
           },
-          "&.Mui-focused fieldset": {
-            borderColor: "flambeeBlue.light",
-          },
-        },
-        "& .MuiOutlinedInput-root.Mui-error": {
-          backgroundColor: theme.surfaces.light.error,
-          "& fieldset": {
-            borderColor: "#B62B35", //TODO is this computed?
-          },
-        },
-      }}
-    >
-      <OutlinedInput
-        id="outlined-input-password"
-        type={values.showPassword ? "text" : "password"}
-        value={values.password}
-        onChange={handleChange("password")}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-              aria-label="toggle password visibility"
-              edge="end"
-            >
-              {values.showPassword ? <IconPasswordSee /> : <IconPasswordHide />}
-            </IconButton>
-          </InputAdornment>
-        }
-        {...props}
-      />
-    </Box>
-  );
-};
+        }}
+      >
+        <OutlinedInput
+          id="outlined-input-password"
+          ref={ref}
+          type={values.showPassword ? "text" : "password"}
+          // value={values.password}
+          // onChange={handleChange("password")}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                aria-label="toggle password visibility"
+                edge="end"
+              >
+                {values.showPassword ? (
+                  <IconPasswordSee />
+                ) : (
+                  <IconPasswordHide />
+                )}
+              </IconButton>
+            </InputAdornment>
+          }
+          {...props}
+        />
+      </Box>
+    );
+  }
+);
 export default PasswordInput;
