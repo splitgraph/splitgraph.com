bbb3c79b296924e9824dd6631173679776e48d25
Patrick Skinner <pas8314@gmail.com>
2021-07-12 12:15:13 +0000

    PasswordButtonInput
    - Composite ('molecule') component that combines <Button> and <Input>
    
    - CU-nn8rwq

--- docs/pages/docs/shelf/index.tsx
+++ docs/pages/docs/shelf/index.tsx
@@ -1,13 +1,57 @@
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper, FormControl } from "@material-ui/core";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import withMuiTheme from "@splitgraph/tdesign/src/themes/withMUITheme";
-import { PasswordInput } from "@splitgraph/tdesign/src/Input";
+import {
+  PasswordInput,
+  ClickToCopyInput,
+  PasswordButtonInput,
+} from "@splitgraph/tdesign/src/Input";
+import { InFieldButton, Button, IconActivity } from "@splitgraph/tdesign";
+import SignInPasswordForm from "../../../components/rhf";
 
 const ThemeDemo = () => {
   return (
-    <Paper>
+    <Paper sx={{ m: "1rem", p: "1rem" }}>
       <Grid item xs={5}>
-        <PasswordInput />
+        <FormControl variant="outlined">
+          <PasswordInput />
+        </FormControl>
+        <br />
         <PasswordInput error />
+        <br />
+        <ClickToCopyInput />
+        <hr />
+        <Button
+          icon={<MailOutlineIcon sx={{ position: "absolute", left: "15px" }} />}
+          large
+        >
+          Complete form to Sign in
+        </Button>
+        <br />
+        <br />
+        <Button disabled>Get Started - FREE</Button>
+        <br />
+        <br />
+        <Button startIcon={<MailOutlineIcon />}>Get Started - FREE</Button>
+        <br />
+        <br />
+        <Button startIcon={<MailOutlineIcon />}>Delete</Button>
+        <br />
+        <br />
+        <br />
+        <InFieldButton>Create</InFieldButton>
+        <br />
+        <br />
+        <InFieldButton disabled>Create</InFieldButton>
+        <br />
+        <br />
+        <br />
+        <PasswordButtonInput />
+        {/* <SignInPasswordForm
+          formErrors={{}}
+          suggestedUsernameOrEmail={"asdf"}
+          redirect={"someUrl"}
+        /> */}
       </Grid>
     </Paper>
   );
