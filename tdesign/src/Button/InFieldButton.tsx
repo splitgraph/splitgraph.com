a1fbd308be40c2f909ee6c0c98144cd8f260d081
Patrick Skinner <pas8314@gmail.com>
2021-07-12 12:00:57 +0000

    InFieldButton component
    - Implements the 'within <input> field button' variant, per Component Library Figma
    - Default, hover, focus, and disabled
    - references the gray colors from design.ts
    - Same approach to `:focus` as Button component (see previous commit msg)
    
    - CU-nnacbn

---
+++ tdesign/src/Button/InFieldButton.tsx
@@ -0,0 +1,38 @@
+import {
+  Button as MuiButton,
+  ButtonProps as MuiButtonProps,
+} from "@material-ui/core";
+import { SxProps } from "@material-ui/system";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+// import { IconArrowRight } from "../Icon";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import { theme } from "../themes/design";
+
+interface IButtonProps extends MuiButtonProps {
+  sx?: SxProps<Theme>;
+}
+const Button = ({ children, disabled, ...rest }: IButtonProps) => (
+  <MuiButton
+    disabled={disabled}
+    sx={{
+      background: disabled
+        ? theme.grays.light.gray26
+        : "linear-gradient(270.8deg, #FF8099 4.38%, #F94569 93.29%)",
+      color: disabled ? theme.grays.light.gray22 : "white",
+      "&:hover": {
+        background: "linear-gradient(270.8deg, #F94569 4.38%, #FF8099 93.29%)",
+        boxShadow: "0px 4px 8px rgba(249, 69, 105, 0.2)",
+      },
+      "&.Mui-disabled": { backgroundColor: theme.grays.light.gray26 },
+    }}
+    // endIcon={}
+    {...rest}
+  >
+    {children}
+    <ArrowRightAltIcon
+      sx={{ ml: "10px", color: disabled ? theme.grays.light.gray22 : "white" }}
+    />
+  </MuiButton>
+);
+
+export default Button;
