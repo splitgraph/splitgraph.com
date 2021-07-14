17650e74913bf4b90bd4d45018b93ed29dacf8f5
Patrick Skinner <pas8314@gmail.com>
2021-07-14 13:49:46 +0000

    Chore: update shelf demos
    WIP

--- docs/pages/docs/shelf/index.tsx
+++ docs/pages/docs/shelf/index.tsx
@@ -5,22 +5,48 @@ import {
   PasswordInput,
   ClickToCopyInput,
   PasswordButtonInput,
+  // ClickToCopySelect,
 } from "@splitgraph/tdesign/src/Input";
-import { InFieldButton, Button, InvisibleButton } from "@splitgraph/tdesign";
+import {
+  InFieldButton,
+  Button,
+  InvisibleButton,
+  LinkButton,
+} from "@splitgraph/tdesign";
+import { useForm } from "react-hook-form";
 // import SignInPasswordForm from "../../../components/rhf";
 
 const ThemeDemo = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Paper sx={{ m: "1rem", p: "1rem" }}>
       <Grid item xs={5}>
         <FormControl variant="outlined">
+          PasswordInput
           <PasswordInput />
         </FormControl>
         <br />
+        PasswordInput (error)
         <PasswordInput error />
         <br />
-        <ClickToCopyInput />
-        <hr />
+        <Paper>
+          ClickToCopyInput
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <ClickToCopyInput {...register("example")} />
+            <input type="submit" />
+          </form>
+        </Paper>
+        {/* ClickToCopySelect */}
+        {/* <ClickToCopySelect /> */}
+        Button (large)
+        <br />
         <Button
           icon={<MailOutlineIcon sx={{ position: "absolute", left: "15px" }} />}
           large
@@ -29,8 +55,13 @@ const ThemeDemo = () => {
         </Button>
         <br />
         <br />
+        Button (disabled)
+        <br />
+        <br />
         <Button disabled>Get Started - FREE</Button>
         <br />
+        <br />
+        Button (startIcon prop)
         <br />
         <Button startIcon={<MailOutlineIcon />}>Get Started - FREE</Button>
         <br />
@@ -39,17 +70,29 @@ const ThemeDemo = () => {
         <br />
         <br />
         <br />
+        {"InFieldButton (used in certain <Input>s)"}
+        <br />
         <InFieldButton>Create</InFieldButton>
         <br />
+        <br />
+        InFieldButton (disabled)
         <br />
         <InFieldButton disabled>Create</InFieldButton>
         <br />
         <br />
         <br />
+        PasswordButtonInput
         <PasswordButtonInput />
         <br />
         <br />
+        so-called "InvisibleButton"
+        <br />
         <InvisibleButton>Set as Primary</InvisibleButton>
+        <br />
+        <br />
+        LinkButton
+        <br />
+        <LinkButton>Sign up</LinkButton>
         {/* <SignInPasswordForm
           formErrors={{}}
           suggestedUsernameOrEmail={"asdf"}
