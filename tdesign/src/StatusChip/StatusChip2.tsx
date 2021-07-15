06f20c70dfc8622e2b706e5adbecd38486f0029d
Patrick Skinner <pas8314@gmail.com>
2021-07-15 22:49:13 +0000

    <StatusChip>
    - Very simple component; StatusChip2 aims to implement the designer's comp
    - Should be a drop in for previous comp. We continue to support passing `message`; special style for 'verified' -> highlight in green
    - One difference is that 'responsiveness' is not addressed in the comp. This component is very small so it may not pose a real issue, something to keep in mind.
    - Also worth remembering that the existing <StatusChip> component is only used in two places
    - Usage demo on /shelf
    - CU-nnazxq

---
+++ tdesign/src/StatusChip/StatusChip2.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from "@material-ui/core";
+import { SxProps } from "@material-ui/system";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import { theme } from "../themes/design";
+export interface IStatusChipProps {
+  text?: string;
+}
+
+const StatusChip2 = ({ text }: IStatusChipProps) => {
+  const isVerified = text?.toLowerCase() === "verified";
+  const containerStyle = {
+    ".status-chip": {
+      padding: "1px 8px 2px",
+      background: (theme) => theme.palette.common.white,
+      border: `1px solid ${isVerified ? "#36CBAE" : theme.grays.light.gray25}`, //TODO: this is called 'on success' but appears to be hardcoded
+      borderRadius: "4px",
+    },
+    ".status-chip--text": {
+      color: isVerified ? "#36CBAE" : theme.grays.light.gray22,
+    },
+  } as SxProps<Theme>;
+
+  return (
+    <Box sx={containerStyle}>
+      <span className="status-chip">
+        <span className="status-chip--text">
+          <Typography variant="smallHighlightedSB">{text}</Typography>
+        </span>
+      </span>
+    </Box>
+  );
+};
+
+export default StatusChip2;
