d1d2331d97d2634dc9fc00e8cc7e969817c8ad92
Patrick Skinner <pas8314@gmail.com>
2021-07-15 22:09:44 +0000

    <ErrorAlert2> and <SuccessAlert2>
    - Original components still avail under their original names for now
    - Brought the designer styles into the v2. Eventually we will swap + replace when we update catalog branch
    - Props are kept backwards compatible to better facilitate this
    - Allow use of `sx` prop to make it easier to override. Since these components are pretty shallow this hopefully will suffice.
    
    - CU-nnakbq

---
+++ tdesign/src/Alert/SuccessAlert2.tsx
@@ -0,0 +1,120 @@
+import { Box, Typography } from "@material-ui/core";
+import { SxProps } from "@material-ui/system";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import MuiLink from "../Link/MuiLink";
+import IconCheckCircle2 from "../Icon/IconCheckCircle2";
+import { theme as design } from "../themes/design";
+
+const Checkmark = () => {
+  return (
+    <Typography sx={{ marginRight: 2, fontWeight: "bold" }}>
+      &#10003;
+    </Typography>
+  );
+};
+
+interface SuccessMessageProps {
+  message?: string;
+}
+
+const SuccessMessage = ({ message }: SuccessMessageProps) => {
+  return <>{message}</>;
+};
+
+interface ISuccessResetLinkProps {
+  text?: string;
+  href?: string;
+  onClick: () => void;
+}
+
+const SuccessResetLink = ({ text, href }: ISuccessResetLinkProps) => (
+  <MuiLink sx={{ color: "#fff" }} href={href || "#"}>
+    {text}
+  </MuiLink>
+);
+
+interface ISuccessAlertProps {
+  message?: string;
+  dismissLinkText?: string;
+  dismissLinkHref?: string;
+  onClickDismiss?: () => void;
+  dismissLinkOwnRow?: boolean;
+  children?: React.ReactNode;
+  sx?: SxProps<Theme>;
+}
+
+const SuccessAlert = ({
+  message,
+  dismissLinkText,
+  dismissLinkHref = null,
+  onClickDismiss,
+  dismissLinkOwnRow = false,
+  children,
+  sx,
+}: ISuccessAlertProps) => {
+  const styles: SxProps<Theme> = {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: 4,
+    borderRadius: "4px",
+    padding: "8px 8px 10px",
+    backgroundColor: design.surfaces.light.success,
+    ...(dismissLinkOwnRow
+      ? {
+          flexWrap: "wrap",
+        }
+      : {}),
+    ...sx,
+  };
+  return (
+    <Box sx={styles}>
+      <Box
+        sx={{
+          display: "flex",
+          width: dismissLinkText && dismissLinkHref && "70%",
+        }}
+      >
+        {message ? (
+          <>
+            <Checkmark />
+            <SuccessMessage message={message} />
+          </>
+        ) : (
+          <>
+            <IconCheckCircle2 aria-label="error" sx={{ mr: "10px" }} />
+            {children}
+          </>
+        )}
+      </Box>
+
+      {dismissLinkText && (dismissLinkHref || onClickDismiss) && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: dismissLinkOwnRow ? "flex-start" : "flex-end",
+          }}
+        >
+          {dismissLinkHref ? (
+            <SuccessResetLink
+              text={dismissLinkText}
+              href={dismissLinkHref || "#"}
+              onClick={onClickDismiss}
+            />
+          ) : (
+            <Typography
+              sx={{
+                color: "muted",
+                ":hover": { cursor: "pointer", textDecoration: "underline" },
+              }}
+              onClick={onClickDismiss}
+            >
+              {dismissLinkText}
+            </Typography>
+          )}
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default SuccessAlert;
--- tdesign/src/Alert/index.tsx
+++ tdesign/src/Alert/index.tsx
@@ -1,3 +1,5 @@
 import ErrorAlert from "./ErrorAlert";
+import ErrorAlert2 from "./ErrorAlert2";
 import SuccessAlert from "./SuccessAlert";
-export { ErrorAlert, SuccessAlert };
+import SuccessAlert2 from "./SuccessAlert2";
+export { ErrorAlert, SuccessAlert, ErrorAlert2, SuccessAlert2 };
