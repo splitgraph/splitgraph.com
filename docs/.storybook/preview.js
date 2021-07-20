20652566f453496bd6c3d00dd03d0fa6fa81169d
Patrick Skinner <pas8314@gmail.com>
2021-07-20 20:17:46 +0000

    Apply MUI's <ThemeProvider> globally for Storybook components
    - In `.storybook/preview.js` exporting a decorators array causes Storybook to run Story components through there.
    - It's a useful place to add MUI (or Emotion or other) <ThemeProvider>
    - I don't expect it to, or have seen it, negatively affect any components that don't expect MUI, but if this happens in the future we could e.g. exempt them or otherwise revisit the global nature of this (sequester MUI-dependent components into their own namespace)

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "@material-ui/core";
+import { muiTheme } from "@splitgraph/tdesign/src/themes/muiTheme";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +10,11 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={muiTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
