98c5e9bc1c677a915adaa1a1e186b74dc5ec7ffb
Patrick Skinner <pas8314@gmail.com>
2021-07-21 11:24:44 +0000

    Fix theme object conflict (affects the Storybook 'Docs' tab)
    - Discovered some kind of theme object/Context conflict imposed by the fact Storybook wraps our components with its own ThemeProvider. (Echoes of Theme-UI vs MUI issue from before)
    - Resolution: learning info from https://github.com/storybookjs/storybook/issues/10231 + a small change https://github.com/storybookjs/storybook/issues/10231#issuecomment-884109533
    - Result: Docs tab now works again

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,4 +1,8 @@
-import { ThemeProvider } from "@material-ui/core";
+import { ThemeProvider as MUIThemeProvider } from "@material-ui/core";
+import { ThemeProvider } from "emotion-theming";
+// Two theme providers is a weird workaround for components that depend on the theme.palette.mode e.g. <PasswordInput>
+// Apparently req'd to support the 'Docs' tab in Storybook. Something to do with Emotion 10 (storybook) and 11 (MUIv5)
+// https://github.com/storybookjs/storybook/issues/10231#issuecomment-613394048, https://github.com/storybookjs/storybook/pull/13300
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { muiTheme } from "@splitgraph/tdesign/src/themes/muiTheme";
 
@@ -14,9 +18,11 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <ThemeProvider theme={muiTheme}>
+    <MUIThemeProvider theme={muiTheme}>
       <CssBaseline />
-      <Story />
-    </ThemeProvider>
+      <ThemeProvider theme={muiTheme}>
+        <Story />
+      </ThemeProvider>
+    </MUIThemeProvider>
   ),
 ];
