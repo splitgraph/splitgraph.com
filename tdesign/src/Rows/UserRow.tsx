1d234438afd7fa1d941a48fdef7adb5e437395cb
Patrick Skinner <pas8314@gmail.com>
2021-07-26 10:24:50 +0000

    <UserRow>
    - Tighten up mobile variant
    - SSR: deliberately avoid using the MUI `useMediaQuery` hook in favor of breakpoints array (or, optionally, <Box>'s display top level prop

--- tdesign/src/Rows/UserRow.tsx
+++ tdesign/src/Rows/UserRow.tsx
@@ -8,6 +8,7 @@ interface IUserRowProps {
   name: string;
   email: string;
   verified: string;
+  metadata?: string;
   handleSetAsPrimary: () => void;
   handleDelete: () => void;
 }
@@ -16,39 +17,86 @@ const UserRow = ({
   name,
   verified,
   email,
+  metadata,
   handleSetAsPrimary,
   handleDelete,
 }: IUserRowProps) => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        p: "1em",
-        boxSizing: "border-box",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.02);",
-        borderRadius: "4px",
-        border: `1px solid ${designTheme.grays.light.gray26};`,
-      }}
-    >
-      <div>
+    <>
+      <Box
+        sx={{
+          display: ["none", "flex"],
+          justifyContent: "space-between",
+          p: "1em",
+          boxSizing: "border-box",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.02);",
+          borderRadius: "4px",
+          border: `1px solid ${designTheme.grays.light.gray26};`,
+        }}
+      >
+        <div>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography variant="subtitle1" sx={{ mr: "8px" }}>
+              {name}
+            </Typography>
+            <StatusChip2 text={verified} />
+          </Box>
+          <Box sx={{ display: "flex" }}>
+            <Typography>{email}</Typography>{" "}
+            {!!metadata && (
+              <Typography>&nbsp;&bull;&nbsp;{metadata}</Typography>
+            )}
+          </Box>
+        </div>
         <Box sx={{ display: "flex", alignItems: "center" }}>
+          <InvisibleButton sx={{ mr: "28px" }} onClick={handleSetAsPrimary}>
+            Set as Primary
+          </InvisibleButton>
+          <Button onClick={handleDelete} name="Delete">
+            <IconDelete />
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: ["block", "none"],
+          p: "1em",
+          boxSizing: "border-box",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.02);",
+          borderRadius: "4px",
+          border: `1px solid ${designTheme.grays.light.gray26};`,
+        }}
+      >
+        <div>
           <Typography variant="subtitle1" sx={{ mr: "8px" }}>
             {name}
           </Typography>
+          <Box sx={{ display: "flex" }}>
+            <Typography>{email}</Typography>{" "}
+            {!!metadata && (
+              <Typography>&nbsp;&bull;&nbsp;{metadata}</Typography>
+            )}
+          </Box>
+        </div>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <StatusChip2 text={verified} />
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <InvisibleButton sx={{ mr: "28px" }} onClick={handleSetAsPrimary}>
+              Set as Primary
+            </InvisibleButton>
+            <Button onClick={handleDelete} name="Delete">
+              <IconDelete />
+            </Button>
+          </Box>
         </Box>
-        <Typography>{email}</Typography>
-      </div>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <InvisibleButton sx={{ mr: "28px" }} onClick={handleSetAsPrimary}>
-          Set as Primary
-        </InvisibleButton>
-        <Button onClick={handleDelete} name="Delete">
-          <IconDelete />
-        </Button>
       </Box>
-    </Box>
+    </>
   );
 };
 
