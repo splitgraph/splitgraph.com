5a1e576c404e7ba49532a38e190e9c573f496c1b
Patrick Skinner <pas8314@gmail.com>
2021-07-29 14:52:28 +0000

    (very experimental) attempting to run chromatic via GHA

---
+++ .ci/chromatic.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CI_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+SPLITGRAPH_DIR="$CI_DIR/.."
+
+pushd "$SPLITGRAPH_DIR"
+
+if test -f tsconfig.ci.json ; then
+    >&2 echo "Build error: tsconfig.ci.json still exists, which means typecheck did not happen"
+    popd
+    exit 1
+fi
+
+if test ! -d dist ; then
+    >&2 echo "Build error: missing dist folder. Did typecheck pass?"
+    popd
+    exit 1
+fi
+
+echo "Temporary quick build" && popd && exit 0
+
+yarn dlx chromatic --project-token=$CHROMATIC_PROJECT_TOKEN
+
+exit 1
