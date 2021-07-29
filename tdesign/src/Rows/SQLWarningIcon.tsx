cfeab70f6e560a335b1c382967717193b8581363
Patrick Skinner <pas8314@gmail.com>
2021-07-29 09:51:33 +0000

    minor: fix attribute typo (console warning)

--- tdesign/src/Rows/SQLWarningIcon.tsx
+++ tdesign/src/Rows/SQLWarningIcon.tsx
@@ -7,7 +7,7 @@ const SQLWarningIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <circle cx="20" cy="20" r="20" fill="white" />
-    <circle cx="20" cy="20" r="20" fill="#2A81F6" fill-opacity="0.13" />
+    <circle cx="20" cy="20" r="20" fill="#2A81F6" fillOpacity="0.13" />
     <path
       d="M21.0207 22.2641H18.9776L18.4607 10.9411H21.5376L21.0207 22.2641ZM21.6361 27.3103H18.3622V24.5041H21.6361V27.3103Z"
       fill="#2A81F6"
