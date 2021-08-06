ebe9347e4c1971013ebbcfc582d12ae734b82621
Patrick Skinner <pas8314@gmail.com>
2021-08-06 23:12:06 +0000

    Query the DDN tweaks
    - <ConnectDDNButton> clean up redundant styles (i.e. defining backgroundColor twice in catalog and .com)
    - pass in sx + {...(rest as any)} to allow customization
    - MUI's Button allows custom components, in this case we want don't want client side link, which is the default
    - more https://next.material-ui.com/guides/routing/#button
    - CU-1c22aff

--- tdesign/src/ConnectDDNButton/ConnectDDNButton.tsx
+++ tdesign/src/ConnectDDNButton/ConnectDDNButton.tsx
@@ -1,21 +1,30 @@
-import { Button } from "@material-ui/core";
+import { Button, Link } from "@material-ui/core";
+import ArrowRightAlt from "@material-ui/icons/ArrowRightAlt";
+import type { SxProps } from "@material-ui/system";
+import type { Theme } from "@material-ui/core/styles";
 
 export interface IConnectDDNButtonProps {
   connectParams?: string;
   children?: React.ReactNode;
   connectURL?: string;
+  sx?: SxProps<Theme>;
 }
 
 const ConnectDDNButton = ({
   connectURL = `/connect`,
   connectParams,
   children,
+  sx,
+  ...rest
 }: IConnectDDNButtonProps) => {
   return (
     <Button
+      component={Link}
       variant="contained"
       href={connectParams ? `${connectURL}?${connectParams}` : `${connectURL}`}
-      sx={{ backgroundColor: "legacySecondary.main" }}
+      sx={sx}
+      endIcon={<ArrowRightAlt />}
+      {...(rest as any)}
     >
       {children || <>Connect to DDN</>}
     </Button>
