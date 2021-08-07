faa9b24d9ff97bca6c3d05f5f609b3f21d1c1375
Miles Richardson <milesrichardson@gmail.com>
2021-08-07 01:29:29 +0100

    Move prism theme (code highlighting) into muiTheme

--- tdesign/src/themes/muiTheme.ts
+++ tdesign/src/themes/muiTheme.ts
@@ -205,6 +205,34 @@ export const muiTheme = createMuiTheme({
     legacySecondary: {
       main: "#89368d",
     },
+    prismTheme: {
+      color: "#e0ffff",
+      backgroundColor: "sgdarkblue.main",
+      ".changed": { color: "#ffd700" },
+      ".operator": { color: "#ffd700" },
+      ".deleted": { color: "#ffa07a77" },
+      ".inserted": { color: "#66cc99" },
+      ".comment": { color: "#81cfe0", fontStyle: "italic" },
+      ".punctuation": { color: "#e0ffff" },
+      ".constant": { color: "#dcc6e0" },
+      ".string, .url": { color: "#00ff7f" },
+      ".variable": { color: "#36d7b7" },
+      ".number, .boolean, .attr-value": { color: "#ffecdb" },
+      ".attr-name": { color: "#ffb454" },
+      ".keyword,.key, .property, .namespace, .tag, .selector, .doctype": {
+        color: "#00ffff",
+      },
+      ".builtin, .char, .constant, .function, .class-name, .symbol": {
+        color: "#ffa07a",
+      },
+      // Used by shell-session
+      ".output": {
+        color: "#e0ffff",
+      },
+      ".important, .language-bash": {
+        color: "#ffa07a",
+      },
+    },
   },
   typography: {
     fontFamily:
@@ -416,6 +444,7 @@ declare module "@material-ui/core/styles/createPalette" {
     light?: PaletteColor;
     link?: PaletteColor;
     legacySecondary?: PaletteColor;
+    prismTheme?: { [key: string]: React.CSSProperties | string };
   }
   interface SurfacePaletteOptions {
     background: PaletteColorOptions;
@@ -486,6 +515,7 @@ declare module "@material-ui/core/styles/createPalette" {
     light?: PaletteColorOptions;
     link?: PaletteColorOptions;
     legacySecondary?: PaletteColorOptions;
+    prismTheme?: { [key: string]: React.CSSProperties | string };
   }
 }
 // Docs on module augmentation for customizing the theme
