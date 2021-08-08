84202b924c2c069e6aea8034c0f8d760bd5c89a7
Miles Richardson <milesrichardson@gmail.com>
2021-08-08 21:27:16 +0100

    Fix circular dependency

--- tdesign/src/Layout/Header/HeaderRight.tsx
+++ tdesign/src/Layout/Header/HeaderRight.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Box } from "@splitgraph/tdesign";
 
 export interface IHeaderRightProps {
   children?: React.ReactNode;
 }
 
 const HeaderRight = ({ children }: IHeaderRightProps) => {
-  return <Box className="header--right">{children}</Box>;
+  return <div className="header--right">{children}</div>;
 };
 
 export default HeaderRight;
