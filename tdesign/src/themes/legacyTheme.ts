67b46e5a02af47bad61d0844037b0461fed42214
Patrick Skinner <pas8314@gmail.com>
2021-08-09 19:44:15 +0000

    Hoisted prismTheme to its own file
    - prismTheme is needed multiple times inside muiTheme, hoist it to avoid circ dep.
    - Used module augmentation similarly to before
    - CU-1c22abk

--- tdesign/src/themes/legacyTheme.ts
+++ tdesign/src/themes/legacyTheme.ts
@@ -1,6 +1,7 @@
 import { SxProps } from "@material-ui/system";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import { defaultTheme } from "./muiTheme";
+import prismTheme from "./prismTheme";
 
 /*
 Pre-April 2021, different areas used different styles provided by different files
@@ -44,35 +45,6 @@ _app: import "@csstools/normalize.css";
 //     color: "rgb(250, 208, 0)",
 //   },
 // };
-
-export const prismTheme = {
-  color: "#e0ffff",
-  backgroundColor: "primary",
-  // backgroundColor: "red",
-  ".changed,.operator": { color: "#ffd700" },
-  ".deleted": { color: "#ffa07a77" },
-  ".inserted": { color: "#66cc99" },
-  ".comment": { color: "#81cfe0", fontStyle: "italic" },
-  ".punctuation": { color: "#e0ffff" },
-  ".constant": { color: "#dcc6e0" },
-  ".string,.url": { color: "#00ff7f" },
-  ".variable": { color: "#36d7b7" },
-  ".number,.boolean,.attr-value": { color: "#ffecdb" },
-  ".attr-name": { color: "#ffb454" },
-  ".keyword,.key,.property,.namespace,.tag,.selector,.doctype": {
-    color: "#00ffff",
-  },
-  ".builtin,.char,.constant,.function,.class-name,.symbol": {
-    color: "#ffa07a",
-  },
-  // Used by shell-session
-  ".output": {
-    color: "#e0ffff",
-  },
-  ".important,.language-bash": {
-    color: "#ffa07a",
-  },
-};
 
 export const asciinemaEmbedStyles = {
   "div.asciinema-embed-container": {
