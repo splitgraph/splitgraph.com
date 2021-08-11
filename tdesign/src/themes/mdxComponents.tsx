6e9aa0e4571a016084ae6bbdeb2b2e08d4769de2
Patrick Skinner <pas8314@gmail.com>
2021-08-11 12:15:45 +0000

    mdxComponents: wire in MUI-hosted palettes for all three variants
    - Minor nit: this repeats the hook call three times for each component, vs one. For DRY reasons it may be preferable to refactor mdxComponents into one component vs just a POJO.

--- tdesign/src/themes/mdxComponents.tsx
+++ tdesign/src/themes/mdxComponents.tsx
@@ -1,23 +1,33 @@
 import { Box } from "@material-ui/core";
-import { makeDefaultTheme } from "./legacyTheme";
+// import { makeDefaultTheme } from "./legacyTheme";
+import { useTheme } from "@material-ui/core/styles";
 
-const defaultTheme = makeDefaultTheme();
+// const defaultTheme = makeDefaultTheme();
 
 export const mdxComponents = {
-  pre: ({ children, ...rest }) => (
-    <Box component="pre" sx={defaultTheme.styles.pre} {...rest}>
-      {children}
-    </Box>
-  ),
-  code: ({ children, ...rest }) => (
-    <Box component="code" sx={defaultTheme.styles.code} {...rest}>
-      {children}
-    </Box>
-  ),
-  inlineCode: ({ children, ...rest }) => (
-    <Box component="code" sx={defaultTheme.styles.inlineCode} {...rest}>
-      {children}
-    </Box>
-  ),
+  pre: ({ children, ...rest }) => {
+    const theme = useTheme();
+    return (
+      <Box component="pre" sx={theme.palette.pre} {...rest}>
+        {children}
+      </Box>
+    );
+  },
+  code: ({ children, ...rest }) => {
+    const theme = useTheme();
+    return (
+      <Box component="code" sx={theme.palette.code} {...rest}>
+        {children}
+      </Box>
+    );
+  },
+  inlineCode: ({ children, ...rest }) => {
+    const theme = useTheme();
+    return (
+      <Box component="code" sx={theme.palette.inlineCode} {...rest}>
+        {children}
+      </Box>
+    );
+  },
 };
 export default mdxComponents;
