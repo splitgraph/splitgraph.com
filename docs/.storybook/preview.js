a58c674b98e567ee955d5b1cb255d2109b24327c
Miles Richardson <milesrichardson@gmail.com>
2021-08-20 08:40:11 +0100

    Remove double ThemeProvider hack
    - It's not necessary anymore AFAICT. PasswordInput looks fine

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,8 +1,4 @@
 import { ThemeProvider as MUIThemeProvider } from "@material-ui/core";
-import { ThemeProvider } from "emotion-theming";
-// Two theme providers is a weird workaround for components that depend on the theme.palette.mode e.g. <PasswordInput>
-// Apparently req'd to support the 'Docs' tab in Storybook. Something to do with Emotion 10 (storybook) and 11 (MUIv5)
-// https://github.com/storybookjs/storybook/issues/10231#issuecomment-613394048, https://github.com/storybookjs/storybook/pull/13300
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { muiTheme } from "@splitgraph/tdesign/src/themes/muiTheme";
 
@@ -26,9 +22,7 @@ export const decorators = [
   (Story) => (
     <MUIThemeProvider theme={muiTheme}>
       <CssBaseline />
-      <ThemeProvider theme={muiTheme}>
-        <Story />
-      </ThemeProvider>
+      <Story />
     </MUIThemeProvider>
   ),
 ];
