7b4947a62bbb2dac67273a150deb0ed8488d83e9
Grzegorz Rozdzialik <grzegorz@splitgraph.com>
2021-11-12 14:58:10 +0000

    Fix `withMUITheme` typing
    `withMUITheme` accepted `any` and returned a component that was not
    fully typed (the main problem was that it did not specify `props`).
    
    This commit changes the HOC typing to forward the type of the passed
    component as the return type.
    
    This makes it so that types for props are correctly exposed on the
    returned component.
    
    It is important when using `withMUITheme` in tests to wrap a component
    and then pass props to it.
    
    Related to CU-1m7bk32

--- tdesign/src/themes/withMUITheme.tsx
+++ tdesign/src/themes/withMUITheme.tsx
@@ -1,4 +1,7 @@
-import { getDisplayName } from "next/dist/next-server/lib/utils";
+import {
+  getDisplayName,
+  NextComponentType,
+} from "next/dist/next-server/lib/utils";
 
 // import { CacheProvider } from "@emotion/react";
 // import createCache from "@emotion/cache";
@@ -8,11 +11,15 @@ import UserTheme from "./UserTheme";
 // If multiple instances of Emotion + caching becomes a problem again, see:
 // https://github.com/emotion-js/emotion/issues/2210#issuecomment-758577800
 
-const withMUITheme = (Page) => {
-  const WithMUITheme = (props) => {
+const withMUITheme = <PageContext extends {}, IP extends {}, Props extends {}>(
+  Page: NextComponentType<PageContext, IP, Props>
+) => {
+  const WithMUITheme: NextComponentType<PageContext, IP, Props> = (props) => {
     return (
       <UserTheme>
-        <Page {...props} />
+        {/* NOTE: Props are not modified in any way - we can safely ignore
+          type-checking error here */}
+        <Page {...(props as any)} />
       </UserTheme>
     );
   };
