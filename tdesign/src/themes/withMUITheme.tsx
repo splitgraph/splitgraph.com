efe7287072c2318234e67da9517af3f1f2557b0f
Grzegorz Rozdzialik <grzegorz@splitgraph.com>
2021-11-12 15:19:23 +0000

    Use `NextPage` instead of `NextComponentType`
    NextPage does not require handling PageContext, which leaves us with a
    simpler implementation.

--- tdesign/src/themes/withMUITheme.tsx
+++ tdesign/src/themes/withMUITheme.tsx
@@ -1,7 +1,5 @@
-import {
-  getDisplayName,
-  NextComponentType,
-} from "next/dist/next-server/lib/utils";
+import type { NextPage } from "next";
+import { getDisplayName } from "next/dist/next-server/lib/utils";
 
 // import { CacheProvider } from "@emotion/react";
 // import createCache from "@emotion/cache";
@@ -11,10 +9,10 @@ import UserTheme from "./UserTheme";
 // If multiple instances of Emotion + caching becomes a problem again, see:
 // https://github.com/emotion-js/emotion/issues/2210#issuecomment-758577800
 
-const withMUITheme = <PageContext extends {}, IP extends {}, Props extends {}>(
-  Page: NextComponentType<PageContext, IP, Props>
+const withMUITheme = <Props extends {}, IP extends {}>(
+  Page: NextPage<Props, IP>
 ) => {
-  const WithMUITheme: NextComponentType<PageContext, IP, Props> = (props) => {
+  const WithMUITheme: NextPage<Props, IP> = (props) => {
     return (
       <UserTheme>
         {/* NOTE: Props are not modified in any way - we can safely ignore
